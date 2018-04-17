d74c3597dbb6b5e41ac23490b2cdab5fa92ee404
Kévin Cadinot <kev.cadinot@gmail.com>
2018-04-17 09:33:55 +0200

    getcontracts add + card

--- app/screens/MyContracts.js
+++ app/screens/MyContracts.js
@@ -1,34 +1,34 @@
 import React, { Component } from "react";
 import { ScrollView, Text, Linking, View } from "react-native";
 import { Card, Button } from "react-native-elements";
-import { Api } from '../../AppApiCall';
+import { GetContracts } from "../api/GetContracts";
 
-const images = [
-    {
-        key: 1,
-        name: "Nathan Anderson",
-        image: require("../images/1.jpg"),
-        url: "https://unsplash.com/photos/C9t94JC4_L8"
-    },
-    {
-        key: 2,
-        name: "Jamison McAndie",
-        image: require("../images/2.jpg"),
-        url: "https://unsplash.com/photos/waZEHLRP98s"
-    },
-    {
-        key: 3,
-        name: "Alberto Restifo",
-        image: require("../images/3.jpg"),
-        url: "https://unsplash.com/photos/cFplR9ZGnAk"
-    },
-    {
-        key: 4,
-        name: "John Towner",
-        image: require("../images/4.jpg"),
-        url: "https://unsplash.com/photos/89PFnHKg8HE"
-    }
-];
+// const images = [
+//     {
+//         key: 1,
+//         name: "Nathan Anderson",
+//         image: require("../images/1.jpg"),
+//         url: "https://unsplash.com/photos/C9t94JC4_L8"
+//     },
+//     {
+//         key: 2,
+//         name: "Jamison McAndie",
+//         image: require("../images/2.jpg"),
+//         url: "https://unsplash.com/photos/waZEHLRP98s"
+//     },
+//     {
+//         key: 3,
+//         name: "Alberto Restifo",
+//         image: require("../images/3.jpg"),
+//         url: "https://unsplash.com/photos/cFplR9ZGnAk"
+//     },
+//     {
+//         key: 4,
+//         name: "John Towner",
+//         image: require("../images/4.jpg"),
+//         url: "https://unsplash.com/photos/89PFnHKg8HE"
+//     }
+// ];
 
 export class MyContracts extends Component{
     static navigationOptions = {
@@ -44,22 +44,22 @@ export class MyContracts extends Component{
                         navigate('FAQ')
                     }
                 />
-                <Api />
-                <ScrollView contentContainerStyle={{paddingVertical: 20}}>
-                    {images.map(({name, image, url, key}) =>
-                        <Card title={`CARD ${key}`} image={image} key={key}>
-                            <Text style={{marginBottom: 10}}>
-                                Photo by {name}.
-                            </Text>
-                            <Button
-                                title={"Go to " + name + "'s profile"}
-                                onPress={() =>
-                                    navigate('ContractDetail', {name: name})
-                                }
-                            />
-                        </Card>
-                    )}
-                </ScrollView>
+                <GetContracts />
+                {/*<ScrollView contentContainerStyle={{paddingVertical: 20}}>*/}
+                    {/*{images.map(({name, image, url, key}) =>*/}
+                        {/*<Card title={`CARD ${key}`} image={image} key={key}>*/}
+                            {/*<Text style={{marginBottom: 10}}>*/}
+                                {/*Photo by {name}.*/}
+                            {/*</Text>*/}
+                            {/*<Button*/}
+                                {/*title={"Go to " + name + "'s profile"}*/}
+                                {/*onPress={() =>*/}
+                                    {/*navigate('ContractDetail', {name: name})*/}
+                                {/*}*/}
+                            {/*/>*/}
+                        {/*</Card>*/}
+                    {/*)}*/}
+                {/*</ScrollView>*/}
             </View>
         )
     }
