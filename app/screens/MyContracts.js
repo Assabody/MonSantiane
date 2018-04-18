f077d410f8f9729f42d6c5815e4006cc6acfc020
Kévin Cadinot <kev.cadinot@gmail.com>
2018-04-18 15:08:37 +0200

    style dans un fichier

--- app/screens/MyContracts.js
+++ app/screens/MyContracts.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {ScrollView, Text, View, StyleSheet, TouchableOpacity} from "react-native";
 import { Card, Button } from "react-native-elements";
+import styles from '../style';
 
 export class MyContracts extends Component{
     constructor(props){
@@ -66,43 +67,3 @@ export class MyContracts extends Component{
         );
     }
 };
-
-
-const styles = StyleSheet.create(
-    {
-        MainContainer:
-            { justifyContent: 'center',
-
-                flex:1,
-                margin: 10
-
-            },
-
-        TextStyle:
-            {
-                fontSize: 18,
-                textAlign: 'center',
-                color: '#000',
-            },
-
-        TitleStyle:
-            {
-                fontSize: 24,
-                textAlign: 'center',
-                color: '#000',
-                paddingBottom: 10,
-            },
-        container:
-            {
-                flex: 1,
-                backgroundColor: '#ffffff',
-                alignItems: 'center',
-                justifyContent: 'center',
-            },
-        buttonClick:
-            {
-                flex: 1,
-                backgroundColor: '#F57E20',
-                marginTop: 8
-            },
-    });
---
+++ app/style.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create(
+    {
+        MainContainer:
+            { justifyContent: 'center',
+
+                flex:1,
+                margin: 10
+            },
+
+        TextStyle:
+            {
+                fontSize: 18,
+                textAlign: 'center',
+                color: '#000',
+            },
+
+        TitleStyle:
+            {
+                fontSize: 24,
+                textAlign: 'center',
+                color: '#000',
+                paddingBottom: 10,
+            },
+        container:
+            {
+                flex: 1,
+                backgroundColor: '#ffffff',
+                alignItems: 'center',
+                justifyContent: 'center',
+            },
+        buttonClick:
+            {
+                flex: 1,
+                backgroundColor: '#F57E20',
+                marginTop: 8
+            },
+    });
